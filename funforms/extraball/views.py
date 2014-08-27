60acdcef840e6351231c30abeaa839d878ef1721
vic <git@dfhu.org>
2014-08-26 20:05:17 -0400

    step through the extraball forms

--- funforms/extraball/views.py
+++ funforms/extraball/views.py
@@ -5,9 +5,10 @@ from django.views.generic import TemplateView, View
 
 
 class ExtraBallView(SessionWizardView):
+    template_name = 'extraball.html'
     def done(self, form_list, **kwargs):
         # TODO: Make this a redirect
-        return render_to_response('done.html', {
+        return render_to_response('extraball-done.html', {
             'form_data': [form.cleaned_data for form in form_list],
         })
 
