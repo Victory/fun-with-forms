2f4763e7d3ea7fe28197d0e5e688980d41096258
vic <git@dfhu.org>
2014-09-02 20:21:56 -0400

    print data form a previous step

--- funforms/extraball/views.py
+++ funforms/extraball/views.py
@@ -7,6 +7,21 @@ from extraball.models import ExtraBallForm1, ExtraBallForm2, ExtraBallForm3
 class ExtraBallView(SessionWizardView):
     form_list =  [ExtraBallForm1, ExtraBallForm2, ExtraBallForm3]
     template_name = 'extraball.html'
+
+
+
+    def get_context_data(self, form, **kwargs):
+        context = super(ExtraBallView, self).get_context_data(form, **kwargs)
+
+        extra_data = {}
+        current_step = int(self.storage.current_step)
+        if current_step == 1:
+            prev_data = self.storage.get_step_data('0')
+            extra_data['subject_val'] = prev_data.get('0-subject','')
+            print "here"
+            context.update(extra_data)
+        return context
+
     def done(self, form_list, **kwargs):
         # TODO: Make this a redirect
         return render_to_response('extraball-done.html', {
