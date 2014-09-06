8e92f16ca31c57d862e4f3989704d0154b8c570a
vic <git@dfhu.org>
2014-09-06 18:44:33 -0400

    flake8

--- funforms/extraball/views.py
+++ funforms/extraball/views.py
@@ -1,13 +1,13 @@
 from django.shortcuts import render_to_response
 from django.contrib.formtools.wizard.views import SessionWizardView
-from django.views.generic import TemplateView, View
+from django.views.generic import TemplateView
 
 from extraball.models import ExtraBallForm1, ExtraBallForm2, ExtraBallForm3
 
-class ExtraBallView(SessionWizardView):
-    form_list =  [ExtraBallForm1, ExtraBallForm2, ExtraBallForm3]
-    template_name = 'extraball.html'
 
+class ExtraBallView(SessionWizardView):
+    form_list = [ExtraBallForm1, ExtraBallForm2, ExtraBallForm3]
+    template_name = 'extraball.html'
 
     def get_form_initial(self, step):
         current_step = int(self.storage.current_step)
@@ -17,11 +17,10 @@ class ExtraBallView(SessionWizardView):
             print "HERE"
             prev_data = self.storage.get_step_data('0')
             if prev_data:
-                subject = prev_data.get('0-subject','')
+                subject = prev_data.get('0-subject', '')
                 initial = {"message": "%s\n----------\n" % subject}
 
         return self.initial_dict.get(step, initial)
-
 
     def get_context_data(self, form, **kwargs):
         context = super(ExtraBallView, self).get_context_data(form, **kwargs)
@@ -31,7 +30,7 @@ class ExtraBallView(SessionWizardView):
 
         if current_step == 2:
             prev_data = self.storage.get_step_data('1')
-            extra_data['message_val'] = prev_data.get('1-message','')
+            extra_data['message_val'] = prev_data.get('1-message', '')
             context.update(extra_data)
 
         return context
@@ -41,6 +40,7 @@ class ExtraBallView(SessionWizardView):
         return render_to_response('extraball-done.html', {
             'form_data': [form.cleaned_data for form in form_list],
         })
+
 
 class DoneView(TemplateView):
     template_name = 'extraball-done.html'
